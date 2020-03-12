00e505a81c1849879d5e77bd981a3ea1d437113c
adrian <skymon65@gmail.com>
2020-03-12 13:26:35 +0100

    Cleaned up the visual code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,8 @@
+import Function.SaveInList;
+import view.TUI;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println((int)'\u200B');
-        System.out.println((int)'\u200C');
-        System.out.println((int)'\u200D');
-        System.out.println((int)'\u200E');
-        System.out.println((int)'\u200F');
-        System.out.println((int)'\u202A');
-        System.out.println((int)'\u202B');
-        System.out.println((int)'\u202C');
-        System.out.println((int)'\u202D');
-        System.out.println((int)'\u202E');
-        System.out.println("a\u200B\u200C\u200D\u200E\u200F\u202A\u202B\u202C\u202D\u202Eb");
+        new TUI(new SaveInList()).work();
     }
 }
